0fd6b8448ed454b012a4acc5a678c60427ccedbe
Mercy Muthoni <ndirangumercy4@gmail.com>
2024-03-21 12:20:11 +0000

    task 0 correction

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- creates a table users with id, email and name and id as primary key
 CREATE TABLE IF NOT EXISTS  users(
-	id NOT NULL AUTO_INCREMENT,
+	id INT NOT NULL AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255),
 	PRIMARY KEY(id)
