9c5c0c54cd6ebd94a7194786ad1ce9454a95b396
Mercy Muthoni <ndirangumercy4@gmail.com>
2024-03-21 13:20:52 +0000

    task 3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,5 @@
 -- lists all bands with Glam rock as their main style
 SELECT band_name,
-       (CASE
-            WHEN split IS NOT NULL THEN (2022 - formed)
-            ELSE (2022 - formed)
-        END) AS lifespan
-FROM metal_bands WHERE main_style = 'Glam rock' ORDER BY lifespan DESC;
+	(IFNULL(split,2022) - formed) AS lifespan
+FROM metal_bands
+WHERE style like '%Glam rock%';
