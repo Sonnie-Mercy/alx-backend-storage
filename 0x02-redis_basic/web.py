01ece2a6de8ffc215b43cf3b48bb50f57454bfe0
Mercy Muthoni <ndirangumercy4@gmail.com>
2024-06-19 15:23:24 +0000

    task 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python3
+""" The web.py module"""
 import requests
 import redis
 import time
 
 # Initialize Redis connection
 redis_conn = redis.Redis()
+
 
 def get_page(url: str) -> str:
     """
@@ -15,16 +18,13 @@ def get_page(url: str) -> str:
     # Check if the page content is cached
     cached_content = redis_conn.get(url)
     if cached_content:
-        return cached_content.decode()
+        return cached_content.decode('utf-8')
 
     # Retrieve HTML content of the URL
     response = requests.get(url)
-    if response.status_code == 200:
-        content = response.text
+    content = response.text
 
-        # Cache the content with expiration time of 10 seconds
-        redis_conn.setex(url, 10, content)
+    # Cache the content with expiration time of 10 seconds
+    redis_conn.setex(url, 10, content)
 
-        return content
-    else:
-        return f"Error: Unable to retrieve content from {url}"
+    return content
